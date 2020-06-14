d49c022e2424c351e77b207b3400549f4f8a1d2a
sifatMoonjerin <moonjerin.sifat@gmail.com>
2020-06-14 12:30:03 +0600

    mergeSort v1

---
+++ prototype/mergeSort.js
@@ -0,0 +1,21 @@
+const mergeSort = (arr) => {
+    if(arr.length <= 1){
+        return arr;
+    }
+
+    const mid = Math.floor(arr.length/2)
+    const a = mergeSort(arr.slice(0, mid));
+    const b = mergeSort(arr.slice(mid, arr.length));
+    // console.log('ddd',a,b)
+
+    let resArr = [];
+
+    while(!!a.length && !!b.length){
+        resArr.push((a[0] < b[0]) ? a.shift() : b.shift())
+    }
+
+    return [...resArr,...a,...b];
+}
+
+const x = mergeSort([1,2,4,3,7,5,6,7,8])
+console.log(x)
