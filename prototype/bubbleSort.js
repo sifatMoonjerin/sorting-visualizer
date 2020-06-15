01950431ebbd3010b9e928ffcdc6fde19ece37ba
sifatMoonjerin <moonjerin.sifat@gmail.com>
2020-06-15 21:09:41 +0600

    bubble v2

--- prototype/bubbleSort.js
+++ prototype/bubbleSort.js
@@ -1,18 +1,18 @@
 const bubbleSort = arr => {
-    let count = 0;
-    let resArr = [];
+    //let count = 0;
+    let steps = [];
     for(let i = 0; i < arr.length-1; i++){
         for(let j = 1; j < arr.length-i; j++){
             if(arr[j-1] > arr[j]){
                 [arr[j-1], arr[j]] = [arr[j], arr[j-1]];
             }
             
-            const newA = new Array(...arr);
-            resArr.push(newA);
-            setTimeout(() => console.log(newA), 1000*count++);
+            // const newA = new Array(...arr);
+            steps.push([...arr]);
+            //setTimeout(() => console.log(newA), 1000*count++);
         }
     }
-    return resArr;
+    return steps;
 }
 
 const x = bubbleSort([2,80,10,49,1]);
