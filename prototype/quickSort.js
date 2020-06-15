98897dd1c2c5f81e7fdff8324521533dcf1de923
sifatMoonjerin <moonjerin.sifat@gmail.com>
2020-06-15 21:08:11 +0600

    quick v3

--- prototype/quickSort.js
+++ prototype/quickSort.js
@@ -5,11 +5,31 @@ const quickSort = unsortedArray => {
         if(start < end){
             const pi = partition(arr, start, end);
 
-            steps.push([...arr]);
+            //steps.push([...arr]);
     
             quick(arr, start, pi-1);
             quick(arr, pi+1, end);
         }
+    }
+
+    const partition = (arr, start, end) => {
+        let pivot = arr[end];
+    
+        let pi = start-1;
+    
+        for(let i = start; i < end; i++){
+            if(arr[i] < pivot){
+                pi++;
+                [arr[i], arr[pi]] = [arr[pi], arr[i]];
+            }
+            steps.push([...arr]);
+        }
+    
+        pi++;
+    
+        [arr[pi], arr[end]] = [arr[end], arr[pi]];
+        return pi;
+    
     }
 
     quick(unsortedArray, 0, unsortedArray.length-1);
@@ -17,24 +37,7 @@ const quickSort = unsortedArray => {
     return steps;
 }
 
-const partition = (arr, start, end) => {
-    let pivot = arr[end];
 
-    let pi = start-1;
-
-    for(let i = start; i < end; i++){
-        if(arr[i] < pivot){
-            pi++;
-            [arr[i], arr[pi]] = [arr[pi], arr[i]];
-        }
-    }
-
-    pi++;
-
-    [arr[pi], arr[end]] = [arr[end], arr[pi]];
-    return pi;
-
-}
 
 
 
