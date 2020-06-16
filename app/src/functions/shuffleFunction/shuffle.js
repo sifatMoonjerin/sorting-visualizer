d5874a689fc57b0172372530292e930749e8ffc7
sifatMoonjerin <moonjerin.sifat@gmail.com>
2020-06-16 11:30:45 +0600

    shuffle v2

--- app/src/functions/shuffleFunction/shuffle.js
+++ app/src/functions/shuffleFunction/shuffle.js
@@ -1,4 +1,10 @@
-const shuffle = arr => {
+const shuffle = num => {
+    const arr = [];
+    const inc = 100/num;
+    for(let i = 0; i < 100; i += inc){
+        arr.push(i);
+    }
+
     for(let i = (arr.length-1); i > 0; i--){
         const j = Math.floor(Math.random() * i);
         [arr[i], arr[j]] = [arr[j], arr[i]];
